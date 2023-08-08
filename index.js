3eb5c0a3cecc89e2e24b500fa68f12102eed38d9
Ashutosh Yadav <ashutosh_yadav@Ashutoshs-MacBook-Air.local>
2023-08-08 16:39:38 +0530

    added fs from node

--- index.js
+++ index.js
@@ -10,7 +10,7 @@
 
 import inquirer from 'inquirer';
 import qr from "qr-image";
-import fs from "fs";
+import fs, { writeFile } from "fs";
 
 inquirer
   .prompt([
@@ -23,6 +23,11 @@ inquirer
     const url  = answers.URL;
     var qr_svg = qr.image(url);
     qr_svg.pipe(fs.createWriteStream('qr_img.png'));
+
+    fs.writeFile('URL.txt' , data , (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!')
+    })
   })
   .catch((error) => {
     if (error.isTtyError) {
